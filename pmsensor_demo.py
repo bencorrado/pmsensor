1988f1e30d2035e6460067cad63ada22698cb10e
Daniel Matuschek <daniel@hifiberry.com>
2016-07-29 15:41:46 +0200

    Simple demo application

--- pmsensor_demo.py
+++ pmsensor_demo.py
@@ -5,19 +5,20 @@ from pmsensor import serial_data_collector as pm
 
 def main():
     logging.basicConfig(level=logging.INFO)
-    dc1 = pm.PMDataCollector("/dev/tty.wchusbserial144740",
-                             pm.SUPPORTED_SENSORS["novafitness,sds021"])
-    dc2 = pm.PMDataCollector("/dev/tty.SLAB_USBtoUART",
-                             pm.SUPPORTED_SENSORS["oneair,s3"],
-                             power_control=pm.DTR_OFF)
+    sensors = []
+    sensors.append(pm.PMDataCollector("/dev/tty.wchusbserial144740",
+                                      pm.SUPPORTED_SENSORS["novafitness,sds011"]))
+#    sensors.append(pm.PMDataCollector("/dev/tty.SLAB_USBtoUART",
+#                                      pm.SUPPORTED_SENSORS["oneair,s3"]))
+    sensors.append(pm.PMDataCollector("/dev/tty.SLAB_USBtoUART4",
+                                      pm.SUPPORTED_SENSORS["plantower,pms7003"]))
 
-    print(dc1.supported_values())
-    print(dc2.supported_values())
+    for s in sensors:
+        print(s.supported_values())
 
     while True:
-        print(dc1.read_data())
-        print(dc2.read_data())
-        print("------")
+        for s in sensors:
+            print(s.read_data())
         time.sleep(300)
 
 if __name__ == '__main__':
