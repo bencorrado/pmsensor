5d5f95ac31bd9f2139513758a79664ae44747589
Daniel Matuschek <daniel@matuschek.net>
2016-08-11 09:03:03 +0200

    Changed demo for different sensors

--- pmsensor_demo.py
+++ pmsensor_demo.py
@@ -6,12 +6,12 @@ from pmsensor import serial_data_collector as pm
 def main():
     logging.basicConfig(level=logging.INFO)
     sensors = []
-    sensors.append(pm.PMDataCollector("/dev/tty.wchusbserial144740",
-                                      pm.SUPPORTED_SENSORS["novafitness,sds011"]))
-#    sensors.append(pm.PMDataCollector("/dev/tty.SLAB_USBtoUART",
-#                                      pm.SUPPORTED_SENSORS["oneair,s3"]))
-    sensors.append(pm.PMDataCollector("/dev/tty.SLAB_USBtoUART4",
-                                      pm.SUPPORTED_SENSORS["plantower,pms7003"]))
+#    sensors.append(pm.PMDataCollector("/dev/tty.wchusbserial144740",
+#                                      pm.SUPPORTED_SENSORS["novafitness,sds011"]))
+    sensors.append(pm.PMDataCollector("/dev/tty.SLAB_USBtoUART",
+                                      pm.SUPPORTED_SENSORS["oneair,s3"]))
+#    sensors.append(pm.PMDataCollector("/dev/tty.SLAB_USBtoUART4",
+#                                      pm.SUPPORTED_SENSORS["plantower,pms7003"]))
 
     for s in sensors:
         print(s.supported_values())
@@ -19,7 +19,7 @@ def main():
     while True:
         for s in sensors:
             print(s.read_data())
-        time.sleep(300)
+        time.sleep(3)
 
 if __name__ == '__main__':
     main()
